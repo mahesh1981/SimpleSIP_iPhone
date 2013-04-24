d5d2727aa52c44900731bba271d2eab08b36e5a3
Chandrasekhar <chandra@techlites.com>
2013-04-24 16:25:10 +0530

    changes

--- RalleeSDK/RalleeSDK.build/Objects-normal/armv7/RalleeVoiceCall.d
+++ RalleeSDK/RalleeSDK.build/Objects-normal/armv7/RalleeVoiceCall.d
@@ -1,15 +1,4 @@
 dependencies: \
-<<<<<<< HEAD
-  /Users/vikashsamota/Desktop/RalleeSDK_iPhone/RalleeSDK/RalleeVoiceCall.m \
-  /Users/vikashsamota/Desktop/RalleeSDK_iPhone/RalleeSDK/RalleeVoiceCall.h \
-  /Users/vikashsamota/Desktop/RalleeSDK_iPhone/RalleeSDK/Rallee.h \
-  /Users/vikashsamota/Desktop/RalleeSDK_iPhone/RalleeSDK/RalleeJSON.h \
-  /Users/vikashsamota/Desktop/RalleeSDK_iPhone/RalleeSDK/RalleeRequest.h \
-  /Users/vikashsamota/Desktop/RalleeSDK_iPhone/RalleeSDK/SupportedFiles/JSON/SBJSON.h \
-  /Users/vikashsamota/Desktop/RalleeSDK_iPhone/RalleeSDK/SupportedFiles/JSON/SBJsonParser.h \
-  /Users/vikashsamota/Desktop/RalleeSDK_iPhone/RalleeSDK/SupportedFiles/JSON/SBJsonBase.h \
-  /Users/vikashsamota/Desktop/RalleeSDK_iPhone/RalleeSDK/SupportedFiles/JSON/SBJsonWriter.h
-=======
   /Users/techlites/Desktop/RalleeSDK_iPhone/RalleeSDK/RalleeVoiceCall.m \
   /Users/techlites/Desktop/RalleeSDK_iPhone/RalleeSDK/RalleeVoiceCall.h \
   /Users/techlites/Desktop/RalleeSDK_iPhone/RalleeSDK/Rallee.h \
@@ -19,4 +8,3 @@ dependencies: \
   /Users/techlites/Desktop/RalleeSDK_iPhone/RalleeSDK/SupportedFiles/JSON/SBJsonParser.h \
   /Users/techlites/Desktop/RalleeSDK_iPhone/RalleeSDK/SupportedFiles/JSON/SBJsonBase.h \
   /Users/techlites/Desktop/RalleeSDK_iPhone/RalleeSDK/SupportedFiles/JSON/SBJsonWriter.h
->>>>>>> fb65094d74daf62d735a27a76963122320a172d6
